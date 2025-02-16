55c4f0cf5cfefb1cdc8dd2109ae140e9d3bc1c21
Reem Soliman <reem.soliman77@gmail.com>
2025-02-16 13:18:36 -0800

    Update EDA- a trip on map.py

--- EDA- a trip on map.py
+++ EDA- a trip on map.py
@@ -17,8 +17,8 @@ import folium
 # Use dbutils secrets to get Snowflake credentials.
 options = {
   "sfUrl": "ab43836.west-europe.azure.snowflakecomputing.com",
-  "sfUser": "rsoliman3@gatech.edu", # Email address of your personal
-  "sfPassword": "subhanAllah2193&", # Password you choose when first login to Snowflake
+  "sfUser": "r****", # Email address of your personal
+  "sfPassword": "", # Password you choose when first login to Snowflake
   "sfDatabase": "GATECH",
   "sfSchema": "GROUP_7", # Replace * by your group number
   "sfWarehouse": "GATECH_WH"
